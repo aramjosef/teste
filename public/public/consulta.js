d9dcef831adc004e5d7d30b43dc8c72a12d31b1f
GabrielBiano <125101449+GabrielAlbinou@users.noreply.github.com>
2024-12-05 19:45:06 -0300

    Add files via upload

---
+++ public/public/consulta.js
@@ -0,0 +1,205 @@
+// Função para consultar cliente
+function consultarCliente(event) {
+    event.preventDefault();
+    const documento = document.getElementById('documento').value;
+    if (documento) {
+        fetch(`/consultarCliente?documento=${documento}`)
+            .then(response => response.json())
+            .then(cliente => {
+                const tbody = document.querySelector('#resultadoConsultaCliente tbody');
+                tbody.innerHTML = '';
+                if (cliente.error) {
+                    alert(cliente.error);
+                } else {
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                        <td>${cliente.nome}</td>
+                        <td>${cliente.email}</td>
+                        <td>${cliente.telefone}</td>
+                        <td>${cliente.endereco}</td>
+                        <td>${cliente.pais}</td>
+                        <td>${cliente.estado}</td>
+                    `;
+                    tbody.appendChild(tr);
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao consultar cliente:', error);
+                alert('Erro ao consultar cliente!');
+            });
+    } else {
+        alert('Por favor, insira um documento válido.');
+    }
+}
+
+// Função para consultar quarto
+function consultarQuarto(event) {
+    event.preventDefault();
+    const numero = document.getElementById('numero').value;
+    if (numero) {
+        fetch(`/consulta_quarto?numero=${numero}`)
+            .then(response => response.json())
+            .then(data => {
+                const tbody = document.querySelector('#resultadoConsultaQuarto tbody');
+                tbody.innerHTML = '';
+                if (data.error) {
+                    alert(data.error);
+                } else {
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                        <td>${data.numero}</td>
+                        <td>${data.status}</td>
+                        <td>${data.tipo_quarto_id}</td>
+                    `;
+                    tbody.appendChild(tr);
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao consultar quarto:', error);
+                alert('Erro ao consultar quarto!');
+            });
+    } else {
+        alert('Por favor, insira um número de quarto válido.');
+    }
+}
+
+// Função para consultar status do quarto
+function consultarStatusQuarto(event) {
+    event.preventDefault();
+    const status = document.getElementById('status').value;
+    fetch(`/consulta_status_quarto?status=${status}`)
+        .then(response => response.json())
+        .then(data => {
+            const tbody = document.querySelector('#resultadoConsultaStatusQuarto tbody');
+            tbody.innerHTML = '';
+            if (data.error) {
+                alert(data.error);
+            } else {
+                data.forEach(quarto => {
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                        <td>${quarto.numero}</td>
+                        <td>${quarto.status}</td>
+                        <td>${quarto.tipo_quarto_id}</td>
+                    `;
+                    tbody.appendChild(tr);
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao consultar status dos quartos:', error);
+            alert('Erro ao consultar status!');
+        });
+}
+
+// Função para consultar data de entrada
+function consultarDataEntrada(event) {
+    event.preventDefault();
+    const dataEntrada = document.getElementById('data_entrada').value;
+    if (dataEntrada) {
+        fetch(`/consulta_data_entrada?data_entrada=${dataEntrada}`)
+            .then(response => response.json())
+            .then(data => {
+                const tbody = document.querySelector('#resultadoConsultaDataEntrada tbody');
+                tbody.innerHTML = '';
+                if (data.error) {
+                    alert(data.error);
+                } else {
+                    data.forEach(checkin => {
+                        const tr = document.createElement('tr');
+                        tr.innerHTML = `
+                            <td>${checkin.id_cliente}</td>
+                            <td>${checkin.numero_quarto}</td>
+                            <td>${checkin.data_en}</td>
+                            <td>${checkin.data_sai}</td>
+                            <td>${checkin.status}</td>
+                        `;
+                        tbody.appendChild(tr);
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao consultar data de entrada:', error);
+                alert('Erro ao consultar data de entrada!');
+            });
+    } else {
+        alert('Por favor, selecione uma data de entrada válida.');
+    }
+}
+
+// Função para consultar data de saída
+function consultarDataSaida(event) {
+    event.preventDefault();
+    const dataSaida = document.getElementById('data_saida').value;
+    if (dataSaida) {
+        fetch(`/consulta_data_saida?data_saida=${dataSaida}`)
+            .then(response => response.json())
+            .then(data => {
+                const tbody = document.querySelector('#resultadoConsultaDataSaida tbody');
+                tbody.innerHTML = '';
+                if (data.error) {
+                    alert(data.error);
+                } else {
+                    data.forEach(checkout => {
+                        const tr = document.createElement('tr');
+                        tr.innerHTML = `
+                            <td>${checkout.id_cliente2}</td>
+                            <td>${checkout.numero_quarto}</td>
+                            <td>${checkout.data_entrada}</td>
+                            <td>${checkout.data_saida}</td>
+                            <td>${checkout.total_pagamento}</td>
+                            <td>${checkout.status}</td>
+                        `;
+                        tbody.appendChild(tr);
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao consultar data de saída:', error);
+                alert('Erro ao consultar data de saída!');
+            });
+    } else {
+        alert('Por favor, selecione uma data de saída válida.');
+    }
+}
+
+async function atualizarCliente() {
+    const documento = document.getElementById('documentoAtualizar').value;
+    const nome = document.getElementById('nomeAtualizar').value;
+    const email = document.getElementById('emailAtualizar').value;
+    const telefone = document.getElementById('telefoneAtualizar').value;
+    const endereco = document.getElementById('enderecoAtualizar').value;
+    const pais = document.getElementById('paisAtualizar').value;
+    const estado = document.getElementById('estadoAtualizar').value;
+
+    if (!documento) {
+        alert('Por favor, insira o documento do cliente a ser atualizado.');
+        return;
+    }
+
+    await fetch('/atualizarCliente', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            documento,
+            nome,
+            email,
+            telefone,
+            endereco,
+            pais,
+            estado
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert('Cliente atualizado com sucesso!');
+        } else {
+            alert(data.error || 'Erro ao atualizar cliente.');
+        }
+    })
+    .catch(error => {
+        console.error('Erro ao atualizar cliente:', error);
+        alert('Erro ao atualizar cliente.');
+    });
+}
---
+++ public/public/script.js
@@ -0,0 +1,202 @@
+
+async function cadastrar_cl() {
+    const na_cl = document.getElementById('name_cl').value;
+    const email_cl = document.getElementById('email_cl').value;
+    const phone_cl = document.getElementById('phone_cl').value;
+    const phoneType_cl = document.getElementById('phoneType_cl').value;
+    const address_cl = document.getElementById('address_cl').value;
+    const Doc_cl = document.getElementById('Doc_cl').value;
+    const tp_doc_cl = document.getElementById('tp_doc_cl').value;
+    const pais_cl = document.getElementById('pais_cl').value;
+    const estado_cl = document.getElementById('estado_cl').value;
+
+    await fetch('/cadastrar_cl', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            na_cl,
+            email_cl,
+            phone_cl,
+            phoneType_cl,
+            address_cl,
+            Doc_cl,
+            tp_doc_cl,
+            pais_cl,
+            estado_cl
+        })
+    });
+
+    alert('Cliente cadastrado com sucesso!');
+}
+
+async function cadastrar_prod() {
+    const desc = document.getElementById('descricao').value;
+    const nome = document.getElementById('nome').value;
+    const forn = document.getElementById('fornecedor').value;
+    const unid = document.getElementById('unidade').value;
+    const data = document.getElementById('data').value;
+    const comp = document.getElementById('preco_compra').value;
+    const vend = document.getElementById('preco_venda').value;
+    const qnt_prod = document.getElementById('quantidade').value;
+
+    await fetch('/cadastrar_prod', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            desc,
+            nome,
+            forn,
+            unid,
+            data,
+            comp,
+            vend,
+            qnt_prod
+        })
+    });
+
+    alert('Produto cadastrado com sucesso!');
+}
+
+async function cadastrar_fornecedor() {
+    const nc = document.getElementById('nome_completo').value;
+    const cnpj = document.getElementById('cnpj').value;
+    const end = document.getElementById('endereco').value;
+
+    await fetch('/cadastrar_fornecedor', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nc, cnpj, end })
+    });
+
+    alert('Fornecedor cadastrado com sucesso!');
+}
+
+async function cadastrar_funcionario() {
+    
+    const func_nm = document.getElementById('nome').value;
+    const func_tell = document.getElementById('tell').value;
+    const func_cpf = document.getElementById('cpf').value;
+    const func_nasc = document.getElementById('data_nascimento').value;
+    const func_end = document.getElementById('endereco').value;
+    const func_cep = document.getElementById('cep').value;
+    const func_sex = document.getElementById('sexo').value;
+    const func_data_ad = document.getElementById('data_admissao').value;
+    const func_set = document.getElementById('setor').value;
+
+    await fetch('/cadastrar_funcionario', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            func_nm,
+            func_tell,
+            func_cpf,
+            func_nasc,
+            func_end,
+            func_cep,
+            func_sex,
+            func_data_ad,
+            func_set
+        })
+    });
+
+    alert('Funcionário cadastrado com sucesso!');
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('id_funcionario').value = gerarIdFuncionario();
+});
+
+async function cadastrar_quarto() {
+    const num_q = document.getElementById('numero_quarto').value
+    const status = document.getElementById('status_quarto').value;
+    const tp_quarto = document.getElementById('tipo_quarto').value;
+    
+
+    await fetch('/cadastrar_quarto', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ num_q, status, tp_quarto })
+    });
+
+    alert('Quarto cadastrado com sucesso!');
+}
+
+async function cadastrar_servico() {
+    const qnt_ser = document.getElementById('quantidade').value;
+    const desc_ser = document.getElementById('descricao').value;
+    const tp_ser = document.getElementById('tipo_servico').value;
+    const id_ser = document.getElementById('id_quarto').value;
+
+    await fetch('/cadastrar_servico', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ qnt_ser, desc_ser, tp_ser, id_ser })
+    });
+
+    alert('Serviço cadastrado com sucesso!');
+}
+
+async function cadastrar_tipo_quarto() {
+    const tp_qto = document.getElementById('tipo_quarto').value;
+    const cara_quarto = document.getElementById('caracteristicas').value;
+    const desc_quarto = document.getElementById('descricao').value;
+    await fetch('/cadastrar_tipo_quarto', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            tp_qto,
+            cara_quarto,
+            desc_quarto
+        })
+    });
+
+    alert('Tipo de quarto cadastrado com sucesso!');
+}
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('id_quarto').value = gerarIdQuarto();
+});
+
+async function cadastrar_ci() {
+    const id_cliente = document.getElementById('id_cl').value;
+    const n_quarto = document.getElementById('numero_quarto').value;
+    const dt_en = document.getElementById('data_en').value;
+    const dt_sa = document.getElementById('data_sa').value;
+    const status = document.getElementById('st').value;
+   
+    await fetch('/cadastrar_ci', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({id_cliente, n_quarto, dt_en, dt_sa, status })
+    });
+
+    alert('Check-in cadastrado com sucesso!');
+}
+async function cadastrar_co() {
+    const id_cliente2 = document.getElementById('id_cl2').value;
+    const n_quarto = document.getElementById('numero_quarto').value;
+    const dt_en = document.getElementById('data_en').value;
+    const dt_sa = document.getElementById('data_sa').value;
+    const total = document.getElementById('tp').value;
+    const status = document.getElementById('st').value;
+
+    await fetch('/cadastrar_co', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            id_cliente2,
+            n_quarto,
+            dt_en,
+            dt_sa,
+            total,
+            status
+        })
+    });
+
+    alert('Check-out cadastrado com sucesso!');
+}
+
+
+
+
+
+
